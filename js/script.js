f8326b9456db3b8fa1e26121cbfdfb3e05f0ca6c
Viktor <example@gmail.com>
2021-08-05 21:35:20 +0300

    firs commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+let incr = 10;
+let decr = 10;
+
+
+// incr++;
+// decr--;
+console.log(++decr);
+console.log(--incr)
+
+
+console.log(2*4===8);
+
+
+const isChecked = false,
+isClose = true;
+
+console.log(isChecked||isClose);
