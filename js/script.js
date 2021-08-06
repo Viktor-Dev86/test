ed6a33b7794ca09a67273d99643206bbd41b18e2
Viktor <viktor.kimster@gmail.com>
2021-08-06 17:16:19 +0300

    house work1

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 "use strict";
 
 
-let incr = 10;
-let decr = 10;
+const numberOfFilms = +prompt("Сколько фильмов Вы уже смотрели?", "221");
+console.log(numberOfFilms);
 
 
-// incr++;
-// decr--;
-console.log(++decr);
-console.log(--incr);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько Вы его оцениваете?", ""),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt("На сколько Вы его оцениваете?", "");
 
 
-console.log(2 * 4 === 8);
-
-
-const isChecked = false,
-    isClose = true;
-
-console.log(isChecked || isClose);
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+    console.log(personalMovieDB);
